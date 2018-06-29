71598fb8124365f9204ca38a9948963b85775f83
ronnie <ronaldndirangu>
2018-06-29 12:37:34 +0300

    Refactored code

--- js/user-details.js
+++ js/user-details.js
@@ -1,15 +1,22 @@
 function getUser() {
-	user_id = localStorage.getItem('user_id')
+	let user_id = localStorage.getItem('user_id')
 	document.getElementById('userid').innerHTML = '<em><b>User ID: </b>'+user_id+'</em>';
 
-	username= localStorage.getItem('username')
+	let username= localStorage.getItem('username')
 	document.getElementById('username').innerHTML = '<em><b>Username: </b>'+username+'</em>';
 
-	email = localStorage.getItem('email')
+	let email = localStorage.getItem('email')
 	document.getElementById('email').innerHTML = '<em><b>Email: </b>'+email+'</em>';
 
-	role = localStorage.getItem('role')
+	let role = localStorage.getItem('role')
 	document.getElementById('role').innerHTML = '<em><b>Role: </b>'+role+'</em>';
+	
+
+	if (role=='true') {
+		console.log(role);
+		document.getElementById('promote').style.display = 'none';
+		document.getElementById('delete').style.display = 'none';
+	}
 
 	document.getElementById('promote').addEventListener('click', promoteUser);
 	document.getElementById('delete').addEventListener('click', deleteUser);
